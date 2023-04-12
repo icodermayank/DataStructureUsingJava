f1554457b72cb65bf8de8bc174e39e5856840f57
Mayank Kumar Sharma <mayanksharma6979@gmail.com>
2023-04-12 17:29:47 +0530

    Learning singly linked list in java

---
+++ SinglyLinkedList.java
@@ -0,0 +1,42 @@
+// first of all we have to define the structure of node using class
+class Node{
+    int data;
+    Node next;
+
+    // create constructor to initilize the data of the node.
+    // Node(int data){
+    //     this.data = data;
+    //     this.next = null;
+    // }
+}
+
+class SinglyLinkedList{
+    public static void main(String [] args){
+
+        // creating head.
+        Node head = new Node();
+
+        // first node.
+        Node first = new Node();
+        Node second = new Node();
+        Node third = new Node();
+
+
+        // assigning data to the nodes.
+        first.data = 4;
+        second.data = 5;
+        third.data = 6;
+
+        // linking node to each other..
+
+        head.next = first;
+        first.next = second;
+        second.next = third;
+        third.next = null;
+
+
+        // printing nodes data 
+
+        
+    }
+}
